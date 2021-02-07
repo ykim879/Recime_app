b158377916d36c57119a2ab50079bd770fb62422
unknown <ykim879@gatech.edu>
2021-02-06 20:17:42 -0800

    try to add ingredients

--- recime/src/app/pantry/pantry.page.ts
+++ recime/src/app/pantry/pantry.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonList} from '@ionic/angular';
 @Component({
   selector: 'app-pantry',
   templateUrl: './pantry.page.html',
   styleUrls: ['./pantry.page.scss'],
 })
 export class PantryPage implements OnInit {
-
+@ViewChild('userIngredientsList', { static: true })  userIngredientsList: IonList;
+  ingredients: any = [];
   constructor() { }
 
   ngOnInit() {
+    this.ingredients = ["potato"]
   }
 
 }
