fba0be06b18a89e824be6e09809ba12c53671a10
unknown <ykim879@gatech.edu>
2021-02-08 11:32:57 -0800

    radio changed it into enum

--- recime/src/app/dietary-restrictions/dietary-restrictions.page.ts
+++ recime/src/app/dietary-restrictions/dietary-restrictions.page.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonRadioGroup } from '@ionic/angular';
+export enum DeitaryRestriction { //maybe have to be in somewhere else
+  TreeNuts = "Tree Nuts",
+  Peanuts = "Peanuts",
+  Milk = "Milk",
+  Glueten = "Glueten",
+  Vegan = "Vegan",
+  Vegetarian = "Vegetarian",
+  Halal = "Halal",
+  Chocolate = "Chocolate",
+  Lactose = "Lactose",
+  Diabetic = "Diabetic"
+}
 @Component({
   selector: 'app-dietary-restrictions',
   templateUrl: './dietary-restrictions.page.html',
   styleUrls: ['./dietary-restrictions.page.scss'],
 })
 export class DietaryRestrictionsPage implements OnInit {
+  @ViewChild('radioGroup') radioGroup: IonRadioGroup //for defualt choice
   defaultHref = '';
+  radioselect = DeitaryRestriction
   constructor() { }
   ionViewDidEnter() {
     this.defaultHref = '../dietary-restrictions/dietary-restrictions.module';
