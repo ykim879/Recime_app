b824a46218b497d67c3a07d7562c6fdec3acd0c1
unknown <ykim879@gatech.edu>
2021-03-04 12:22:04 -0800

    change visual + connect range with backend

--- recime/src/app/filters/filters.page.ts
+++ recime/src/app/filters/filters.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.page.scss'],
 })
 export class FiltersPage implements OnInit {
-
-  constructor() { }
+  time: Object;
+  timeMin: any;
+  timeMax: any;
+//https://forum.ionicframework.com/t/getting-current-value-of-a-ion-range/70938/2
+  constructor() { 
+    this.time = {
+      upper: 0,
+      lower: 30
+    };// later get it from user data
+  }
 
   ngOnInit() {
   }
-
+  setBadge(time) {
+    this.timeMin = time.lower;
+    this.timeMax = time.upper;
+}
 }
