23a0c050978d3ca1a9562517748c03c59a7b587f
unknown <ykim879@gatech.edu>
2021-03-04 22:43:02 -0800

    filter page finished

--- recime/src/app/filters/filters.page.ts
+++ recime/src/app/filters/filters.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserData } from '../user-data';
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.page.html',
@@ -9,6 +9,10 @@ export class FiltersPage implements OnInit {
   time: Object;
   timeMin: any;
   timeMax: any;
+  courses: Set<string> = new Set(); //initialize with filter's component's elements
+  cuisines: Set<string> = new Set(); //initialize with filter's component's 
+  ingredients: Set<string> = new Set(); //initialize with filter's component's elements
+  userIngredients: Set<string> = new Set();//initialize this from the storage
 //https://forum.ionicframework.com/t/getting-current-value-of-a-ion-range/70938/2
   constructor() { 
     this.time = {
@@ -18,9 +22,32 @@ export class FiltersPage implements OnInit {
   }
 
   ngOnInit() {
+    //initialize user ingredients from the storage
   }
   setBadge(time) {
     this.timeMin = time.lower;
-    this.timeMax = time.upper;
+    this.timeMax = time.upper; // it can be deleted --> can access via time
+}
+selectCourse(course) {
+  this.courses.add(course);
+  console.log(this.courses);
+}
+deleteCourse(course) {
+  this.courses.delete(course);
+}
+selectCuisine(cuisine) {
+  this.cuisines.add(cuisine);
+}
+deleteCuisine(course) {
+  this.cuisines.delete(course);
+}
+selectIngredients(ingredient) {
+  this.ingredients.add(ingredient);
+}
+deleteIngredients(ingredient) {
+  this.ingredients.delete(ingredient);
+}
+saveSkills() {
+//To Do!
 }
 }
